ca9b61e140561ff08b58f052b153e53fffdded97
Alex <252459@gmail.com>
2022-12-07 12:04:15 +0300

    Решение задачи

---
+++ HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+string[] finalArray = new string[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Array.Resize(ref finalArray, finalArray.Length + 1);
+        finalArray[finalArray.Length - 1] = array[i];
+        System.Console.WriteLine(finalArray[finalArray.Length - 1]);
+    }
+
+}
